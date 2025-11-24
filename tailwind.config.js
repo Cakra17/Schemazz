8e961c8b897c7a9e441bb0fce25ae66a8d6a75e4
theerlant <ttrinanda2@gmail.com>
2025-11-25 05:45:42 +0800

    deleted the wrong module lol

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+      darkMode: 'class', // or 'media' or 'selector'
+      theme: {
+        extend: {
+            fontFamily: {
+                jb: ['JetBrains-Mono', 'mono']
+            }
+        }
+      }
+    }
