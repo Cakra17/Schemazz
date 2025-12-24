38727f7a0c5e1da7d6fa8cee3c847e0ffee431c7
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-12-25 00:22:40 +0800

    chore: remove unused variables

--- src/components/ReactFlowDiagram.tsx
+++ src/components/ReactFlowDiagram.tsx
@@ -1,46 +1,56 @@
 import {
-	Background,
-	ConnectionMode,
-	Controls,
-	Panel,
-	ReactFlow,
-	ReactFlowProvider,
-	type Edge,
+  Background,
+  ConnectionMode,
+  Controls,
+  Panel,
+  ReactFlow,
+  ReactFlowProvider,
 } from "@xyflow/react";
-import type { TableNodeType } from "./diagram/NodeType";
-import TableNode, { defaultColorSelection } from "./diagram/TableNode";
+import TableNode from "./diagram/TableNode";
 import { SchemaStore } from "@/store/node-store";
 import { Button } from "./primitives/Button";
+import { FileStore } from "@/store/file-store";
 
 const nodeTypes = {
-	table: TableNode,
+  table: TableNode,
 };
 
 export default function ReactFlowDiagram() {
-	const { nodes, edges, onNodesChange, onEdgesChange, resetSchema } = SchemaStore();
+  const { nodes, edges, onNodesChange, onEdgesChange, resetSchema } =
+    SchemaStore();
+  const { resetText } = FileStore();
 
-	return (
-		<ReactFlowProvider>
-			<div className="absolute h-full w-full rounded-2xl overflow-hidden bg-gray-50 z-2">
-				<ReactFlow
-					colorMode="dark"
-					className=" dark:border-1 dark:border-neutral-600 rounded-2xl"
-					nodeTypes={nodeTypes}
-					nodes={nodes}
-					edges={edges}
-					onNodesChange={onNodesChange}
-					onEdgesChange={onEdgesChange}
-					connectionMode={ConnectionMode.Loose}
-				>
-					<Panel position="top-left">
-						<Button variant="danger" className="text-[1rem]" onClick={resetSchema}> 
-							Reset
-						</Button>
-					</Panel>
-					<Controls />
-					<Background gap={12} size={1} />
-				</ReactFlow>
-			</div>
-		</ReactFlowProvider>
-	);
+  const handleReset = () => {
+    resetText();
+    resetSchema();
+  };
+
+  return (
+    <ReactFlowProvider>
+      <div className="absolute h-full w-full rounded-2xl overflow-hidden bg-gray-50 z-2">
+        <ReactFlow
+          colorMode="dark"
+          className=" dark:border-1 dark:border-neutral-600 rounded-2xl"
+          nodeTypes={nodeTypes}
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          connectionMode={ConnectionMode.Loose}
+        >
+          <Panel position="top-left">
+            <Button
+              variant="danger"
+              className="text-[1rem]"
+              onClick={handleReset}
+            >
+              Reset
+            </Button>
+          </Panel>
+          <Controls />
+          <Background gap={12} size={1} />
+        </ReactFlow>
+      </div>
+    </ReactFlowProvider>
+  );
 }
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,97 +1,9 @@
 import EditorHeader from "@/components/EditorHeader";
 import ReactFlowDiagram from "@/components/ReactFlowDiagram";
-import { defaultColorSelection } from "@/components/diagram/TableNode";
-import { useState } from "react";
-import { NoobSQLParser } from "@/lib/parser";
-import { SchemaStore } from "@/store/node-store";
-import type { Position, Table } from "@/types/node";
-import type { TextArea } from "@/types/editor";
-import type { Edge } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import CustomEditor from "@/components/CustomEditor";
 
-const Value = {
-  editor: ""
-}
-
-export default function Editor() {
-	const { addTable, addEdge, resetSchema } = SchemaStore();
-	const [ data, setData ] = useState<TextArea>(Value);
-	const parser = new NoobSQLParser();
-
-	const handleSql = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		const { name, value } = event.target;
-		setData((prevData) => ({...prevData, [name]: value}));
-
-		try {
-			parser.SetSQLText(value);
-			parser.Parse();
-			const asts = parser.GetAST();
-
-			resetSchema();
-
-			let x = 100;
-			let y = 100;
-			let spacing = 500;
-
-			for (const ast of asts) {
-				const table: Table = {
-					columns: ast.column,
-					name: ast.tableName,
-					headerColor: defaultColorSelection["gray"],
-				}
-				const position: Position = { x, y };
-				addTable(table, position, "table");
-
-				x += spacing;
-				if (x > window.innerWidth - 400) {
-					x = 100;
-					y += spacing;
-				}
-			}
-
-      const currentNodes = SchemaStore.getState().nodes;
-      let counter = 1;
-      
-      for (const ast of asts) {
-        if (ast.relation.length === 0) continue;
-        
-        ast.relation.forEach((rl) => {
-          const source = currentNodes.find((node) => 
-            node.data.table.name === rl.from.tableName
-          );
-          const target = currentNodes.find((node) => 
-            node.data.table.name === rl.to.tableName
-          );
-          
-          if (!source || !target) return;
-
-          const sourceHandle = source.data.table.columns.find((col) => 
-            col.name === rl.from.columnName
-          );
-          const targetHandle = target.data.table.columns.find((col) => 
-            col.name === rl.to.columnName
-          );
-          
-          if (!sourceHandle || !targetHandle) return;
-
-          const edge: Edge = {
-            id: `edge-${counter++}`,
-            source: source.id,
-            target: target.id,
-            sourceHandle: sourceHandle.id,
-            targetHandle: targetHandle.id,
-            type: "smoothstep",
-            animated: true,
-            style: { stroke: "#00d9ff", strokeWidth: 2 },
-          }
-          addEdge(edge);
-        });
-      }
-		} catch (error) {
-			console.log(error);
-		}	
-	};
-
+export default function EditorPage() {
 	const handleResizeStart = (e: React.MouseEvent) => {
 		const startX = e.clientX;
 		const startWidth = parseInt(
@@ -127,13 +39,7 @@ export default function Editor() {
 					className="relative bg-stone-900 pr-1 transition-all duration-10 flex flex-col"
 					style={{ width: "var(--panel-width, 400px)" }}
 				>
-					<textarea
-						name="editor"
-						id="editor"
-						className="flex-1 w-full resize-none bg-stone-800 text-white jb p-2 rounded-2xl focus:outline-none "
-						onChange={handleSql}
-						defaultValue={data.editor}
-					></textarea>
+					<CustomEditor/>
 				</aside>
 				<div
 					className="w-1 bg-stone-900 hover:bg-[#00d9ff] rounded-full cursor-col-resize transition-colors flex-none"
